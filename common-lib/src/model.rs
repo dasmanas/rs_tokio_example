8c0eab5679fbc7297a98a241293761b765bd8db0
Manas Das <manasd@jfrog.com>
2023-01-31 23:59:33 -0800

    initial checkin

---
+++ common-lib/src/model.rs
@@ -0,0 +1,22 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Zipcode {
+    #[serde(rename = "post code")]
+    pub post_code: String,
+    pub country: String,
+    #[serde(rename = "country abbreviation")]
+    pub country_abbreviation: String,
+    pub places: Vec<Place>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Place {
+    #[serde(rename = "place name")]
+    pub place_name: String,
+    pub longitude: String,
+    pub state: String,
+    #[serde(rename = "state abbreviation")]
+    pub state_abbreviation: String,
+    pub latitude: String,
+}
